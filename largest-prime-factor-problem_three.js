92a9075d13a2d32bcaaa0104eacf92bc1245c950
fabio <fabgmad@gmail.com>
2020-12-09 19:51:15 -0300

    Problem Three Solution - Minor Changes

--- largest-prime-factor-problem_three.js
+++ largest-prime-factor-problem_three.js
@@ -1,11 +1,10 @@
 /* eslint-disable no-console */
 /* eslint-disable max-len */
 /* eslint-disable no-restricted-syntax */
+
 /* The prime factors of 13195 are 5, 7, 13 and 29.
 
 What is the largest prime factor of the number 600851475143 ? */
-
-// let targetValue = 600851475143;
 
 const findLargestPrimeFactor = (targetNumber) => {
   const primeFactors = [];
