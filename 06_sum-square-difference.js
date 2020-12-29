01e629bc74033454ff49a02c3d84a3bf7b2b03a1
fabio <fabgmad@gmail.com>
2020-12-29 17:30:58 -0300

    Problem Six Resolution

---
+++ 06_sum-square-difference.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-console */
+/* eslint-disable max-len */
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-return-assign */
+
+const createNumbersIntervalRepository = (args) => {
+  const initialNumber = args[0];
+  const finalNumber = args[1];
+  const numbersIntervalRepository = [];
+  for (let index = initialNumber; index <= finalNumber; index += 1) {
+    numbersIntervalRepository.push(index);
+  }
+  return numbersIntervalRepository;
+};
+
+const calcSumOfSquares = (args) => {
+  const numbersIntervalRepository = createNumbersIntervalRepository(args);
+  const sumOfSquares = numbersIntervalRepository.reduce((accumulator, currentValue) => accumulator += currentValue ** 2, 0);
+  return sumOfSquares;
+};
+
+const calcSquareOfSum = (args) => {
+  const numbersIntervalRepository = createNumbersIntervalRepository(args);
+  const squareOfSum = numbersIntervalRepository.reduce((accumulator, currentValue) => accumulator += currentValue, 0) ** 2;
+  return squareOfSum;
+};
+
+const calcSumSquareDifference = (...args) => {
+  const minAndMaxValues = args;
+  const sumOfSquares = calcSumOfSquares(minAndMaxValues);
+  const squareOfSum = calcSquareOfSum(minAndMaxValues);
+  const sumSquareDifference = Math.max(squareOfSum - sumOfSquares, 0);
+  return sumSquareDifference;
+};
+
+calcSumSquareDifference(1, 100);
